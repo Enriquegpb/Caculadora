26c93c6f237c4c1982d56f5d8dbb002e9e31ba94
Enrique <fpemlktz@gmail.com>
2021-10-20 10:01:36 +0200

    Calculadora parte grafica version original

---
+++ app/src/main/java/com/enrique/caculadora/MainActivity.java
@@ -0,0 +1,97 @@
+package com.enrique.caculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    /*public Button btnsumar;
+    public Button btnrestar;
+    public Button btnmultiplicar;
+    public Button btndividir;
+    public Button btnreset;
+    public TextView txtN1;
+    public TextView txtN2;
+    public TextView txtResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnsumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n1=0;
+                double n2=0;
+                double resultado=0;
+
+                n1=Double.parseDouble(txtN1.getText().toString());
+                n2=Double.parseDouble(txtN2.getText().toString());
+
+                resultado=n1+n2;
+
+                txtResultado.setText(String.valueOf(resultado));
+            }
+        });
+
+        btnrestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n1=0;
+                double n2=0;
+                double resultado=0;
+
+                n1=Double.parseDouble(txtN1.getText().toString());
+                n2=Double.parseDouble(txtN2.getText().toString());
+
+                resultado=n1-n2;
+
+                txtResultado.setText(String.valueOf(resultado));
+            }
+        });
+
+        btnmultiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n1=0;
+                double n2=0;
+                double resultado=0;
+
+                n1=Double.parseDouble(txtN1.getText().toString());
+                n2=Double.parseDouble(txtN2.getText().toString());
+
+                resultado=n1*n2;
+
+                txtResultado.setText(String.valueOf(resultado));
+            }
+        });
+
+        btndividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n1=0;
+                double n2=0;
+                double resultado=0;
+
+                n1=Double.parseDouble(txtN1.getText().toString());
+                n2=Double.parseDouble(txtN2.getText().toString());
+
+                resultado=n1/n2;
+
+                txtResultado.setText(String.valueOf(resultado));
+            }
+        });
+
+        btndividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtN1.setText(null);
+                txtN2.setText(null);
+                txtResultado.setText(null);
+            }
+        });
+    }*/
+}
