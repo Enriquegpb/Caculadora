5936d7ea52f800fbba118359d5c8180ea0d046b7
Enrique <fpemlktz@gmail.com>
2021-10-30 14:47:47 +0200

    creados botonos numericos

--- app/src/main/java/com/enrique/caculadora/MainActivity.java
+++ app/src/main/java/com/enrique/caculadora/MainActivity.java
@@ -9,16 +9,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1=(Button) findViewById(R.id.button10);
+        btn2=(Button) findViewById(R.id.button11);
+        btn3=(Button) findViewById(R.id.button12);
+        btn4=(Button) findViewById(R.id.button7);
+        btn5=(Button) findViewById(R.id.button8);
+        btn6=(Button) findViewById(R.id.button9);
+        btn7=(Button) findViewById(R.id.button4);
+        btn8=(Button) findViewById(R.id.button5);
+        btn9=(Button) findViewById(R.id.button6);
+        tv=(TextView) findViewById(R.id.textView);
     }
 
-    int op1, op2, resultado;
-    String operador;
-
-    public void Resultado(View view) {
+    public void MetodoCalcularResultado(View view) {
+        int op1, op2, resultado=0;
+        String operador=null;
         TextView tvcalc = (TextView) findViewById(R.id.textView);
         op1 = Integer.parseInt(tvcalc.getText().toString());
         op2 = Integer.parseInt(tvcalc.getText().toString());
