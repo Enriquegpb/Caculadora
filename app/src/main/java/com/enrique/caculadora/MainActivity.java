440c3b5f054ab5e1030b56f50bba0077ac7cc3ac
Enrique <fpemlktz@gmail.com>
2021-11-04 21:01:52 +0100

    setText y GetText version 0 numericos y coma decimal

--- app/src/main/java/com/enrique/caculadora/MainActivity.java
+++ app/src/main/java/com/enrique/caculadora/MainActivity.java
@@ -8,14 +8,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnigual,btnsuma,btnresta,btnmultiplicacion,btndivision,btnc,btndel,btncoma;
+    Button btn0,btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnigual,btnsuma,btnresta,btnmultiplicacion,btndivision,btnc,btndel,btncoma;
     TextView tv;
-    String operador,cad;
+    String operador,op1="",op2="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn0 = (Button) findViewById(R.id.button18);
         btn1 = (Button) findViewById(R.id.button10);
         btn2 = (Button) findViewById(R.id.button11);
         btn3 = (Button) findViewById(R.id.button12);
@@ -35,63 +35,82 @@ public class MainActivity extends AppCompatActivity {
         btnigual = (Button) findViewById(R.id.button16);
         tv = (TextView) findViewById(R.id.textView);
 
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("0");
+                tv.getText();
+            }
+        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
+                tv.setText("1");
+                tv.getText();
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                tv.setText("2");
+                tv.getText();
             }
         });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                tv.setText("3");
+                tv.getText();
             }
         });
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                tv.setText("4");
+                tv.getText();
             }
         });
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                tv.setText("5");
+                tv.getText();
             }
         });
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                tv.setText("6");
+                tv.getText();
             }
         });
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                tv.setText("7");
+                tv.getText();
             }
         });
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                tv.setText("8");
+                tv.getText();
             }
         });
         btn9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                tv.setText("9");
+                tv.getText();
+            }
+        });
+        btncoma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(".");
+                tv.getText();
             }
         });
         btnsuma.setOnClickListener(new View.OnClickListener() {
@@ -118,16 +137,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-
+        
     }
 
     public int MetodoCalcularResultado(int op1, int op2, String operador) {
         int resultado = 0;
-        TextView tvcalc = (TextView) findViewById(R.id.textView);
-        op1 = Integer.parseInt(tvcalc.getText().toString());
-        op2 = Integer.parseInt(tvcalc.getText().toString());
+        op1 = Integer.parseInt(tv.getText().toString());
+        op2 = Integer.parseInt(tv.getText().toString());
 
         switch (operador) {
             case "+":
