b7ab2eeb863d1aab87c720dec425d63a40f4fafb
Enrique <fpemlktz@gmail.com>
2021-11-16 12:38:30 +0100

    Calculadora mçModo Oscuro Final

--- app/src/main/java/com/enrique/caculadora/MainActivity.java
+++ app/src/main/java/com/enrique/caculadora/MainActivity.java
@@ -1,16 +1,21 @@
 package com.enrique.caculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn0,btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnigual,btnsuma,btnresta,btnmultiplicacion,btndivision,btnc,btndel,btncoma;
     TextView tv;
     String operador,op1="",op2="";
+    Switch sw;
+    ConstraintLayout cl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
         btncoma=(Button) findViewById(R.id.button17);
         btnigual = (Button) findViewById(R.id.button16);
         tv = (TextView) findViewById(R.id.textView);
-
+        sw=(Switch)findViewById(R.id.sw);
+        cl=(ConstraintLayout)findViewById(R.id.contenedor);
         btn0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -158,7 +164,95 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(tv.getText().subSequence(0,tv.getText().length()-1));
             }
         });
-
+        sw.setBackgroundColor(Color.GRAY);
+        sw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sw.isChecked()){
+                    sw.setText("on");
+                    sw.setBackgroundColor(Color.GRAY);
+                    cl.setBackgroundColor(Color.DKGRAY);
+                    btn0.setBackgroundColor(Color.YELLOW);
+                    btn1.setBackgroundColor(Color.YELLOW);
+                    btn2.setBackgroundColor(Color.YELLOW);
+                    btn3.setBackgroundColor(Color.YELLOW);
+                    btn4.setBackgroundColor(Color.YELLOW);
+                    btn5.setBackgroundColor(Color.YELLOW);
+                    btn6.setBackgroundColor(Color.YELLOW);
+                    btn7.setBackgroundColor(Color.YELLOW);
+                    btn8.setBackgroundColor(Color.YELLOW);
+                    btn9.setBackgroundColor(Color.YELLOW);
+                    btnsuma.setBackgroundColor(Color.YELLOW);
+                    btnresta.setBackgroundColor(Color.YELLOW);
+                    btnmultiplicacion.setBackgroundColor(Color.YELLOW);
+                    btndivision.setBackgroundColor(Color.YELLOW);
+                    btnigual.setBackgroundColor(Color.YELLOW);
+                    btncoma.setBackgroundColor(Color.YELLOW);
+                    btndel.setBackgroundColor(Color.YELLOW);
+                    btnc.setBackgroundColor(Color.YELLOW);
+                    btn0.setTextColor(Color.DKGRAY);
+                    btn1.setTextColor(Color.DKGRAY);
+                    btn2.setTextColor(Color.DKGRAY);
+                    btn3.setTextColor(Color.DKGRAY);
+                    btn4.setTextColor(Color.DKGRAY);
+                    btn5.setTextColor(Color.DKGRAY);
+                    btn6.setTextColor(Color.DKGRAY);
+                    btn7.setTextColor(Color.DKGRAY);
+                    btn8.setTextColor(Color.DKGRAY);
+                    btn9.setTextColor(Color.DKGRAY);
+                    btnsuma.setTextColor(Color.DKGRAY);
+                    btnresta.setTextColor(Color.DKGRAY);
+                    btnmultiplicacion.setTextColor(Color.DKGRAY);
+                    btndivision.setTextColor(Color.DKGRAY);
+                    btnc.setTextColor(Color.DKGRAY);
+                    btncoma.setTextColor(Color.DKGRAY);
+                    btndel.setTextColor(Color.DKGRAY);
+                    btnigual.setTextColor(Color.DKGRAY);
+                    tv.setTextColor(Color.WHITE);
+                }
+                else{
+                    sw.setText("off");
+                    sw.setBackgroundColor(Color.WHITE);
+                    cl.setBackgroundColor(Color.WHITE);
+                    btn0.setBackgroundColor(Color.BLUE);
+                    btn1.setBackgroundColor(Color.BLUE);
+                    btn2.setBackgroundColor(Color.BLUE);
+                    btn3.setBackgroundColor(Color.BLUE);
+                    btn4.setBackgroundColor(Color.BLUE);
+                    btn5.setBackgroundColor(Color.BLUE);
+                    btn6.setBackgroundColor(Color.BLUE);
+                    btn7.setBackgroundColor(Color.BLUE);
+                    btn8.setBackgroundColor(Color.BLUE);
+                    btn9.setBackgroundColor(Color.BLUE);
+                    btnsuma.setBackgroundColor(Color.BLUE);
+                    btnresta.setBackgroundColor(Color.BLUE);
+                    btnmultiplicacion.setBackgroundColor(Color.BLUE);
+                    btndivision.setBackgroundColor(Color.BLUE);
+                    btnigual.setBackgroundColor(Color.BLUE);
+                    btncoma.setBackgroundColor(Color.BLUE);
+                    btndel.setBackgroundColor(Color.BLUE);
+                    btnc.setBackgroundColor(Color.BLUE);
+                    btn0.setTextColor(Color.WHITE);
+                    btn1.setTextColor(Color.WHITE);
+                    btn2.setTextColor(Color.WHITE);
+                    btn3.setTextColor(Color.WHITE);
+                    btn4.setTextColor(Color.WHITE);
+                    btn5.setTextColor(Color.WHITE);
+                    btn6.setTextColor(Color.WHITE);
+                    btn7.setTextColor(Color.WHITE);
+                    btn8.setTextColor(Color.WHITE);
+                    btn9.setTextColor(Color.WHITE);
+                    btnsuma.setTextColor(Color.WHITE);
+                    btnresta.setTextColor(Color.WHITE);
+                    btnmultiplicacion.setTextColor(Color.WHITE);
+                    btndivision.setTextColor(Color.WHITE);
+                    btnc.setTextColor(Color.WHITE);
+                    btncoma.setTextColor(Color.WHITE);
+                    btndel.setTextColor(Color.WHITE);
+                    btnigual.setTextColor(Color.WHITE);
+                }
+            }
+        });
     }
 
     public Double MetodoCalcularResultado(double op1, double op2, String operador) {
